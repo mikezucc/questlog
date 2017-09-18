3af1845a231411ea144b26638630d00a91296df9
Michael Zuccarino <mikezuccarino@gmail.com>
2017-09-18 10:08:28 -0700

    Thoughts

---
+++ requirementes.py
@@ -0,0 +1,31 @@
+# OSX
+911  git status
+912  git pull origin master
+913  python manage.py migrate
+914  ls
+915  pip install Django
+916  sudo pip install Django
+917  sudo pip install --upgrade Django
+918  python manage.py migrate
+920  pip install boto
+921  python manage.py migrate
+922  pip install magic
+923  pip install python-magic
+924  python manage.py migrate
+925  pip install libmagic
+926  python manage.py migrate
+927  pip install libmagic
+928  sudo port install file
+929  brew install file-formula
+930  python manage.py migrate
+931  history
+932 pip install protobuf
+933 pip install enum
+933 pip install google-cloud
+
+sudo pip install --upgrade google-api-python-client
+sudo pip install --upgrade google-cloud
+sudo pip install --upgrade google-cloud-vision
+
+934 pip install Pillow
+ls
