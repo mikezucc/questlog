fbd89cb7b658e999dc2fd886a31d8a3360e1588e
Michael Zuccarino <mikezuccarino@gmail.com>
2017-12-03 16:16:00 -0800

    Come on now git old chap

---
+++ requ/requirementes.py
@@ -0,0 +1,53 @@
+# OSX
+911  git status
+912  git pull origin master
+913  python manage.py migrate
+914  ls
+915  pip install Django
+916  sudo pip install Django
+917  sudo pip install --upgrade Django
+918  python manage.py migrate
+920  pip install boto
+921  python manage.py migrate
+922  pip install magic
+923  pip install python-magic
+924  python manage.py migrate
+925  pip install libmagic
+926  python manage.py migrate
+927  pip install libmagic
+928  sudo port install file
+929  brew install file-formula
+930  python manage.py migrate
+931  history
+932 pip install protobuf
+933 pip install enum
+933 pip install google-cloud
+
+sudo pip install --upgrade google-api-python-client
+sudo pip install --upgrade google-cloud
+sudo pip install --upgrade google-cloud-vision
+
+934 sudo pip install Pillow
+ls
+sudo pip install boto3
+
+
+sudo pip install boto
+sudo pip install magic
+sudo pip install python-magic
+sudo pip install libmagic
+sudo sudo port install file
+sudo brew install file-formula
+sudo pip install protobuf
+sudo pip install enum
+sudo pip install google-cloud
+sudo pip install --upgrade google-api-python-client
+sudo pip install --upgrade google-cloud
+sudo pip install --upgrade google-cloud-vision
+sudo pip install Pillow
+sudo pip install boto3
+
+#if youre using fucking brew and system installation at the same time, sometimes brew can be tarded
+export PYTHONPATH=$PYTHONPATH:/Library/Python/2.7/site-packages/django/bin/django-admin.py
+
+export DJANGO_SETTINGS_MODULE="questlog.settings"
